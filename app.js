aeed46cf35ac11df01197b39f9888a03dc209724
Ren F <renfinn89@gmail.com>
2020-03-19 13:33:19 -0700

    added prevent

--- app.js
+++ app.js
@@ -1,123 +1,142 @@
-/**
- * Example store structure
- */
-const store = {
-  // 5 or more questions are required
-  questions: [
-    {
-      question: 'What color is broccoli?',
-      answers: [
-        'red',
-        'orange',
-        'pink',
-        'green'
-      ],
-      correctAnswer: 'green'
-    },
-    {
-      question: 'What is the current year?',
-      answers: [
-        '1970',
-        '2015',
-        '2019',
-        '2005'
-      ],
-      correctAnswer: '2019'
-    }
-  ],
-  quizStarted: false,
-  questionNumber: 0,
-  score: 0
-};
+// /**
+//  * Example store structure
+//  */
+// const store = {
+//   // 5 or more questions are required
+//   questions: [
+//     {
+//       question: 'Who is the cutest in all the Galaxy?',
+//       answers: [
+//         'Babu Frik',
+//         'Baby Yoda',
+//         'R2-D2',
+//         'Salacius B. Crumb'
+//       ],
+//       correctAnswer: 'Baby Yoda'
+//     },
+//     {
+//       question: 'What was the order issued to kill the Jedi?',
+//       answers: [
+//         '66',
+//         '347',
+//         '42',
+//         '47'
+//       ],
+//       correctAnswer: '66'
+//     }
+//     {
+//       question: 'What color was Mace Windu\'s lightsaber?',
+//       answers: [
+//         'Blue',
+//         'Green',
+//         'Red',
+//         'Purple'
+//       ],
+//       correctAnswer: 'Purple'
+//     },
+//     {
+//       question: 'Which hand did Luke have cut off?',
+//       answers: [
+//         'Left',
+//         'I thought it was a foot!',
+//         'Right',
+//         'SHe didn\'t have a hand cut off'
+//       ],
+//       correctAnswer: 'Right'
+//     },
+//     {
+//       question: 'What was Finn\'s stormtropper code name?',
+//       answers: [
+//         'JK-1983',
+//         'FN-2187',
+//         'GN-6783',
+//         'FK-3902'
+//       ],
+//       correctAnswer: 'FN-2187'
+//     },
+//   ],
+//   quizStarted: false,
+//   questionNumber: 0,
+//   score: 0
+// };
 
-
-$("#my-home-nav").click(function(){
-  $("#my-home-div").show(); // or .fadeIn(), etc.
-  $("#my-about-div").hide();
-  $("#my-contact-div").hide();
+$('button').on('click', function(event) {
+  event.preventDefault();
+  console.log('Working');
 });
 
+// $("#my-home-nav").click(function(){
+//   $("#my-home-div").show(); // or .fadeIn(), etc.
+//   $("#my-about-div").hide();
+//   $("#my-contact-div").hide();
+// });
 
+// // START PAGE
+// $('.Start').on('click', 'Start-Button', function handleStartButton () {
+//   $(nextpage).show()
+// }
 
-// START PAGE
-$('.Start').on('click', 'Start-Button', function handleStartButton () {
-  $(nextpage).show()
-}
+// function getQuestion(item) {
+//   return $(question)
+//     .closest('li')
+//     .data('item-id');
+// }
 
+// function getAnswer(item) {
+//   return $(answer)
+//     .closest('li')
+//     .data('item-id');
+// }
 
+// //QUESTION PAGE
 
-function getQuestion(item) {
-  return $(question)
-    .closest('li')
-    .data('item-id');
-}
+// function toggleCheckedAnswer() {
 
-function getAnswer(item) {
-  return $(answer)
-    .closest('li')
-    .data('item-id');
-}
+// }
 
+// function handleNextButton(page) {
 
+// }
 
-//QUESTION PAGE
+// function handleAnswer() {
 
-function toggleCheckedAnswer() {
+// }
 
+// function checkIfCorrect() {
 
-}
+// }
 
+// //CORRECT
 
-function handleNextButton(page) {
+// function handleScore() {
 
+// }
 
-}
+// function
 
+// /**
+//  *
+//  * Technical requirements:
+//  *
+//  * Your app should include a render() function, that regenerates the view each time the store is updated.
+//  * See your course material, consult your instructor, and reference the slides for more details.
+//  *
+//  * NO additional HTML elements should be added to the index.html file.
+//  *
+//  * You may add attributes (classes, ids, etc) to the existing HTML elements, or link stylesheets or additional scripts if necessary
+//  *
+//  * SEE BELOW FOR THE CATEGORIES OF THE TYPES OF FUNCTIONS YOU WILL BE CREATING 👇
+//  *
+//  */
 
-function handleAnswer() {
-  
+// /********** TEMPLATE GENERATION FUNCTIONS **********/
 
-}
+// // These functions return HTML templates
 
+// /********** RENDER FUNCTION(S) **********/
 
-function checkIfCorrect() {
+// // This function conditionally replaces the contents of the <main> tag based on the state of the store
 
+// /********** EVENT HANDLER FUNCTIONS **********/
 
-}
-
-
-//CORRECT
-
-function handleScore() {
-
-
-}
-
-function 
-
-/**
- * 
- * Technical requirements:
- * 
- * Your app should include a render() function, that regenerates the view each time the store is updated. 
- * See your course material, consult your instructor, and reference the slides for more details.
- *
- * NO additional HTML elements should be added to the index.html file.
- *
- * You may add attributes (classes, ids, etc) to the existing HTML elements, or link stylesheets or additional scripts if necessary
- *
- * SEE BELOW FOR THE CATEGORIES OF THE TYPES OF FUNCTIONS YOU WILL BE CREATING 👇
- * 
- */
-
-/********** TEMPLATE GENERATION FUNCTIONS **********/
-
-// These functions return HTML templates
-
-/********** RENDER FUNCTION(S) **********/
-
-// This function conditionally replaces the contents of the <main> tag based on the state of the store
-
-/********** EVENT HANDLER FUNCTIONS **********/
-
-// These functions handle events (submit, click, etc)
+// // These functions handle events (submit, click, etc)
